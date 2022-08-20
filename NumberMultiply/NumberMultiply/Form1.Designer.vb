5a2227dc6a77008e450cfff788dc54320b15599a
keinsaza78 <keinsaza1@gmail.com>
2022-08-20 21:38:07 +0700

    .

--- NumberMultiply/NumberMultiply/Form1.Designer.vb
+++ NumberMultiply/NumberMultiply/Form1.Designer.vb
@@ -23,22 +23,83 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.btOk = New System.Windows.Forms.Button()
+        Me.tbNum = New System.Windows.Forms.TextBox()
+        Me.btClear = New System.Windows.Forms.Button()
+        Me.btExit = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label1
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(147, 71)
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label1.Font = New System.Drawing.Font("Showcard Gothic", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(107, 38)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(53, 20)
+        Me.Label1.Size = New System.Drawing.Size(468, 86)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Label1"
+        Me.Label1.Text = "ตารางสูตรคูณ"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'btOk
+        '
+        Me.btOk.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btOk.Location = New System.Drawing.Point(80, 343)
+        Me.btOk.Name = "btOk"
+        Me.btOk.Size = New System.Drawing.Size(120, 57)
+        Me.btOk.TabIndex = 1
+        Me.btOk.Text = "ตกลง"
+        Me.btOk.UseVisualStyleBackColor = True
+        '
+        'tbNum
+        '
+        Me.tbNum.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.tbNum.Location = New System.Drawing.Point(379, 212)
+        Me.tbNum.Name = "tbNum"
+        Me.tbNum.Size = New System.Drawing.Size(185, 27)
+        Me.tbNum.TabIndex = 2
+        '
+        'btClear
+        '
+        Me.btClear.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btClear.Location = New System.Drawing.Point(273, 343)
+        Me.btClear.Name = "btClear"
+        Me.btClear.Size = New System.Drawing.Size(122, 57)
+        Me.btClear.TabIndex = 1
+        Me.btClear.Text = "ยกเลิก"
+        Me.btClear.UseVisualStyleBackColor = True
+        '
+        'btExit
+        '
+        Me.btExit.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btExit.Location = New System.Drawing.Point(463, 343)
+        Me.btExit.Name = "btExit"
+        Me.btExit.Size = New System.Drawing.Size(127, 57)
+        Me.btExit.TabIndex = 1
+        Me.btExit.Text = "จบการทำงาน"
+        Me.btExit.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label2.Font = New System.Drawing.Font("Showcard Gothic", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(107, 176)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(179, 63)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "ใส่ตัวเลข"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.TopCenter
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(627, 476)
+        Me.ClientSize = New System.Drawing.Size(701, 494)
+        Me.Controls.Add(Me.tbNum)
+        Me.Controls.Add(Me.btExit)
+        Me.Controls.Add(Me.btClear)
+        Me.Controls.Add(Me.btOk)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -48,4 +109,9 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents Label1 As Label
+    Friend WithEvents btOk As Button
+    Friend WithEvents tbNum As TextBox
+    Friend WithEvents btClear As Button
+    Friend WithEvents btExit As Button
+    Friend WithEvents Label2 As Label
 End Class
