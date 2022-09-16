61142ebfa44c17b36e673c88495660bd362817f5
keinsaza78 <keinsaza1@gmail.com>
2022-09-16 11:23:59 +0700

    แก้ไขใหม่หมดของเพื่อนแก้มันหาย

--- NumberMultiply/NumberMultiply/Form1.Designer.vb
+++ NumberMultiply/NumberMultiply/Form1.Designer.vb
@@ -32,6 +32,7 @@ Partial Class Form1
         '
         'Label1
         '
+        Me.Label1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer))
         Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.Label1.Font = New System.Drawing.Font("Showcard Gothic", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
         Me.Label1.Location = New System.Drawing.Point(107, 38)
@@ -39,17 +40,18 @@ Partial Class Form1
         Me.Label1.Size = New System.Drawing.Size(468, 86)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "ตารางสูตรคูณ"
-        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'btOk
         '
+        Me.btOk.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
         Me.btOk.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
         Me.btOk.Location = New System.Drawing.Point(80, 343)
         Me.btOk.Name = "btOk"
         Me.btOk.Size = New System.Drawing.Size(120, 57)
         Me.btOk.TabIndex = 1
         Me.btOk.Text = "ตกลง"
-        Me.btOk.UseVisualStyleBackColor = True
+        Me.btOk.UseVisualStyleBackColor = False
         '
         'tbNum
         '
@@ -61,26 +63,29 @@ Partial Class Form1
         '
         'btClear
         '
+        Me.btClear.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
         Me.btClear.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
         Me.btClear.Location = New System.Drawing.Point(273, 343)
         Me.btClear.Name = "btClear"
         Me.btClear.Size = New System.Drawing.Size(122, 57)
         Me.btClear.TabIndex = 1
         Me.btClear.Text = "ยกเลิก"
-        Me.btClear.UseVisualStyleBackColor = True
+        Me.btClear.UseVisualStyleBackColor = False
         '
         'btExit
         '
+        Me.btExit.BackColor = System.Drawing.Color.Red
         Me.btExit.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
         Me.btExit.Location = New System.Drawing.Point(463, 343)
         Me.btExit.Name = "btExit"
         Me.btExit.Size = New System.Drawing.Size(127, 57)
         Me.btExit.TabIndex = 1
         Me.btExit.Text = "จบการทำงาน"
-        Me.btExit.UseVisualStyleBackColor = True
+        Me.btExit.UseVisualStyleBackColor = False
         '
         'Label2
         '
+        Me.Label2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
         Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.Label2.Font = New System.Drawing.Font("Showcard Gothic", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
         Me.Label2.Location = New System.Drawing.Point(107, 176)
@@ -94,6 +99,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.AppWorkspace
         Me.ClientSize = New System.Drawing.Size(701, 494)
         Me.Controls.Add(Me.tbNum)
         Me.Controls.Add(Me.btExit)
--- NumberMultiply/NumberMultiply/Form1.vb
+++ NumberMultiply/NumberMultiply/Form1.vb
@@ -1,3 +1,41 @@
 ﻿Public Class Form1
+    Private Sub btOk_Click(sender As Object, e As EventArgs) Handles btOk.Click
+        Dim Num, sum, i As Integer
+        Dim ans As String
+        ans = ""
+        If tbNum.Text = "" Then
+            MsgBox("กรุณาใส่ข้อมูลตัวเลขก่อน", MsgBoxStyle.Exclamation, "ข้อมูลผิดพลาด")
+            tbNum.Focus()
+        Else
+            If IsNumeric(tbNum.Text) Then
+                Num = Val(tbNum.Text)
+                For i = 1 To 12
+                    sum = Num * i
+                    ans += Num & "x" & i & " = " & sum & vbCrLf
+                Next i
+                MsgBox(ans)
+            Else
+                MsgBox("กรุณาใส่ข้อมูลตัวเลขเท่านั้น", MsgBoxStyle.Exclamation, "ข้อมูลผิดพลาด")
+                tbNum.Focus()
+            End If 'check numeric error
 
+        End If 'check blank error
+    End Sub
+
+    Private Sub btClear_Click(sender As Object, e As EventArgs) Handles btClear.Click
+        tbNum.Text = ""
+        tbNum.Focus()
+    End Sub
+
+    Private Sub btExit_Click(sender As Object, e As EventArgs) Handles btExit.Click
+        Dim bt_Exit As MsgBoxResult
+        bt_Exit = MsgBox("จบการทำงานใช่หรือไม่ ?", MsgBoxStyle.YesNo, "จบการทำงาน")
+        If bt_Exit = MsgBoxResult.Yes Then
+            MsgBox("เลิกการทำงาน.....", MsgBoxStyle.Exclamation, "จบการทำงาน")
+            Application.Exit()
+        Else
+            bt_Exit = MsgBoxResult.No
+            tbNum.Focus()
+        End If
+    End Sub
 End Class
